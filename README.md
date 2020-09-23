4d065a091f353c0cfe9ec6a1bfb18f0d7d24f0ea
TehGM <wojop95@gmail.com>
2020-09-23 17:13:51 +0200

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SubBot-CS
 C# instance of SubBot
+
+## Running
+1. Download or clone.
+2. Add `appsecrets.json` file (with "Copy always" or "Copy if newer" for **Copy to Output Directory**).
+3. Populate with secrets. See [appsecrets-example.json](appsecrets-example.json) for example.
+4. Build and run.
+
+## License
+Copyright (c) 2020 DevSubmarine
+
+Licensed under [Apache 2.0 License](LICENSE).
---
+++ SubBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace DevSubmarine.SubBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("appsecrets.json", optional: true);
+                    config.AddJsonFile($"appsecrets.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    // configure options
+
+                    // add framework services
+
+                    // add Discord client
+
+                    // add handlers
+
+                })
+                .Build();
+            await host.RunAsync().ConfigureAwait(false);
+        }
+    }
+}
