ba51377fdfcb47c5350f07fbbd7fe9ecc722b9ad
ANF-Studios <theanfstudios@gmail.com>
2020-09-27 22:06:10 +0500

    Added and updated Administrator commands
    - Fixed some errors in Ban and kick command
    - Added Purge messages and Slowmode command

---
+++ SubBot/Main/Commands/Administrator/Ban.cs
@@ -0,0 +1,53 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace DevSubmarine.SubBot.Commands.Administrator
+{
+    public class Ban : ModuleBase<SocketCommandContext>
+    {
+        [RequireOwner]
+        [RequireBotPermission(GuildPermission.BanMembers, ErrorMessage = "I do not have permission: `Ban Members`")]
+        [Command("ban")]
+        public async Task BanUser(IGuildUser user = null, string reason = "Not specified!", [Remainder] int pruneDays = 0)
+        {
+            if (user == null)
+            {
+                Embed nullUser = new EmbedBuilder()
+                    .WithColor(Color.Red)
+                    .WithTitle("Ban Command : Info")
+                    .AddField("Usage:", "`ban <user> <reason*> <pruneMessages* (days)>`", true)
+                    .WithDescription("Bans a user from this server.")
+                    .WithFooter("*Optional parameters")
+                    .Build();
+                await Context.Message.Channel.SendMessageAsync(embed: nullUser);
+            }
+
+            else
+            {
+                try
+                {
+                    await user.BanAsync(pruneDays, reason);
+
+                    Embed successBan = new EmbedBuilder()
+                        .WithColor(Color.Green)
+                        .WithAuthor("Ban Command : Success", user.GetAvatarUrl())
+                        .WithDescription($"Successfully banned {user.Mention}")
+                        .Build();
+                    await Context.Message.Channel.SendMessageAsync(embed: successBan);
+                }
+                catch (Exception ex)
+                {
+                    Embed errorBan = new EmbedBuilder()
+                        .WithColor(Color.Red)
+                        .WithTitle("Ban Command : Error")
+                        .WithDescription(ex.Message)
+                        .WithFooter($"{ex.HResult}")
+                        .Build();
+                    await Context.Message.Channel.SendMessageAsync(embed: errorBan);
+                }
+            }
+        }
+    }
+}
---
+++ SubBot/Main/Commands/Administrator/Kick.cs
@@ -0,0 +1,53 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace DevSubmarine.SubBot.Commands.Administrator
+{
+    public class Kick : ModuleBase<SocketCommandContext>
+    {
+        [RequireOwner]
+        [RequireUserPermission(GuildPermission.KickMembers, ErrorMessage = "You do not have permission: `Kick Members`")]
+        [Command("kick")]
+        public async Task KickUser(IGuildUser user = null, string reason = "Not specified!")
+        {
+            if (user == null)
+            {
+                Embed nullUser = new EmbedBuilder()
+                    .WithColor(Color.Red)
+                    .WithTitle("Kick Command : Info")
+                    .AddField("Usage:", "`kick <user> <reason*>`", true)
+                    .WithDescription("Kicks a user from this guild.")
+                    .WithFooter("*Optional parameters")
+                    .Build();
+                await Context.Message.Channel.SendMessageAsync(embed: nullUser);
+            }
+
+            else
+            {
+                try
+                {
+                    await user.KickAsync(reason);
+
+                    Embed successKick = new EmbedBuilder()
+                        .WithColor(Color.Green)
+                        .WithAuthor("Kick Command : Success", user.GetAvatarUrl())
+                        .WithDescription($"Successfully banned {user.Mention}")
+                        .Build();
+                    await Context.Message.Channel.SendMessageAsync(embed: successKick);
+                }
+                catch (Exception ex)
+                {
+                    Embed errorKick = new EmbedBuilder()
+                        .WithColor(Color.Red)
+                        .WithTitle("Kick Command : Error")
+                        .WithDescription(ex.Message)
+                        .WithFooter($"{ex.HResult}")
+                        .Build();
+                    await Context.Message.Channel.SendMessageAsync(embed: errorKick);
+                }
+            }
+        }
+    }
+}
